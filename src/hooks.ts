ed5a5a4ce5819743d532bdde9db7c8c384711187
Alex Kovac <261950131+captcharesolver@users.noreply.github.com>
2025-07-09 23:36:00 +0000

    feat: add metrics endpoint

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,4 +1,4 @@
-// updated: 2025-07-10T05:27:00Z
+// updated: 2025-07-09T23:36:00Z
 // module: src/hooks.ts
-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 
