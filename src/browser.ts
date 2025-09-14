3e65f94cb1a8f70b168f2aa4ffd54336777269e0
Alex Kovac <261950131+captcharesolver@users.noreply.github.com>
2025-09-14 19:21:00 +0000

    docs: add API reference

--- src/browser.ts
+++ src/browser.ts
@@ -1,4 +1,4 @@
-// updated: 2025-09-05T17:25:00Z
+// updated: 2025-09-14T19:21:00Z
 // module: src/browser.ts
-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+xxxxxxxxxxxxxxxxxxxxxxxxx
 
