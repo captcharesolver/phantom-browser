a467ccf4d92719eb6bc6ec586fb3050bb95714a0
Alex Kovac <261950131+captcharesolver@users.noreply.github.com>
2025-10-22 03:55:00 +0000

    chore: bump node version

--- tests/browser.test.ts
+++ tests/browser.test.ts
@@ -1,4 +1,4 @@
-// updated: 2025-10-21T23:21:00Z
+// updated: 2025-10-22T03:55:00Z
 // module: tests/browser.test.ts
-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 
