85bb936041b5ab84972cbc3aa3acc53854cc397a
Alex Kovac <261950131+captcharesolver@users.noreply.github.com>
2025-10-24 18:14:00 +0000

    refactor: extract config parser

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,4 @@
-// updated: 2025-10-02T00:04:00Z
+// updated: 2025-10-24T18:14:00Z
 // module: src/utils.ts
-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 
