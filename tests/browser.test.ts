4445a49e97ea0c6fa9efd611ca8962b41cfcde18
Alex Kovac <261950131+captcharesolver@users.noreply.github.com>
2025-10-27 18:52:00 +0000

    feat: add webhook notifications

--- tests/browser.test.ts
+++ tests/browser.test.ts
@@ -1,4 +1,4 @@
-// updated: 2025-10-22T03:55:00Z
+// updated: 2025-10-27T18:52:00Z
 // module: tests/browser.test.ts
-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 
