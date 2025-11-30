0206fe0e67299f2fee5879257b2e67ef9e9efdba
Alex Kovac <261950131+captcharesolver@users.noreply.github.com>
2025-11-30 06:38:00 +0000

    chore: bump node version

--- src/session.ts
+++ src/session.ts
@@ -1,4 +1,4 @@
-// updated: 2025-11-11T18:33:00Z
+// updated: 2025-11-30T06:38:00Z
 // module: src/session.ts
-xxxxxxxxxxxx
+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 
